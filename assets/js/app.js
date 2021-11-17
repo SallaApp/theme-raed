c224b865a1fea534e144b028084faf8ec2842253
Salah Alkhwlani <salah@salla.sa>
2021-11-17 15:04:28 +0300

    ✨ New Awesome Developing Session

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 require('mmenu-light');
 require('./partials/custom')
 require('./partials/search-modal')
-require('@sallaapp/theme-utils')
+require('@sallaapp/theme-utils') // todo :: change the namespace to salla.sa
+require('@salla.sa/twilight-components')
+
 window.anime = require('animejs').default;
 import Alpine from 'alpinejs'
 window.Alpine = Alpine;
