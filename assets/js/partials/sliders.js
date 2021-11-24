91412bb441dd7a90bd25e6daeb0219e189de90c3
Ashraf Reda <ashraf.reda.m@gmail.com>
2021-11-24 17:10:32 +0200

    home blocks responsive fixes

--- assets/js/partials/sliders.js
+++ assets/js/partials/sliders.js
@@ -1,4 +1,3 @@
-
 /**
  * #1 Default Product slider ------------------------------------------------------
  * Used in:
@@ -8,20 +7,67 @@
  */
 
 document.addEventListener('DOMContentLoaded', function () {
-  const sliders = document.querySelectorAll(".product-slider");
+	const sliders = document.querySelectorAll(".product-slider");
 
-  sliders.forEach((slider) => {
-    const sliderId = '#' + slider.dataset.id;
-    const next = document.querySelector(sliderId + ' .slider-next');
-    const prev = document.querySelector(sliderId + ' .slider-prev');
+	sliders.forEach((slider) => {
+		const sliderId = '#' + slider.dataset.id;
+		const next = document.querySelector(sliderId +
+			' .slider-next');
+		const prev = document.querySelector(sliderId +
+			' .slider-prev');
 
-    const swiper = new Swiper(slider, {
-      slidesPerView: 'auto',
-      draggable: true,
-      navigation: {
-        nextEl: next,
-        prevEl: prev,
-      },
-    });
-  });
+		const swiper = new Swiper(slider, {
+			slidesPerView: 'auto',
+			draggable: true,
+			navigation: {
+				nextEl: next,
+				prevEl: prev,
+			},
+		});
+	});
+});
+
+/**
+ * #2 Photos slider ------------------------------------------------------
+ * Used in:
+ *  1- photos_slider.twig, 
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+	const sliders = document.querySelectorAll(".photos-slider");
+
+	sliders.forEach((slider) => {
+		const sliderId = '#' + slider.dataset.id;
+		const next = document.querySelector(sliderId + ' .swiper-button-next');
+		const prev = document.querySelector(sliderId + ' .swiper-button-prev');
+		const pagination = document.querySelector(sliderId + ' .swiper-pagination');
+
+		const swiper = new Swiper(slider, {
+			slidesPerView: "auto",
+			loop: true,
+			centeredSlides: true,
+      preloadImages: false,
+      lazy: true,
+			spaceBetween: 30,
+			pagination: {
+				el: pagination,
+				clickable: true,
+			},
+			navigation: {
+				nextEl: next,
+				prevEl: prev,
+			},
+			breakpoints: {
+				320: {
+					spaceBetween: 10,
+				},
+				768: {
+					spaceBetween: 15,
+				},
+				980: {
+					paceBetween: 30,
+				},
+			}
+		});
+	});
 });
