ea479835945e7cb35e47fa00f6ece3985b012323
Ashraf Reda <ashraf.reda.m@gmail.com>
2021-12-22 11:48:42 +0200

    wishlist buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ require('./partials/cart-listeners');
 require('./partials/helpers');
 require('./partials/sliders');
 require('./partials/home-blocks');
+require('./partials/wishlist');
 // require('./partials/search-modal')
 require('@salla.sa/twilight/components')
 require('lite-youtube-embed');
-
