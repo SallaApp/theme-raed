3bcdfb6fae0d9f88bd96ea102a84eecb447a2428
Ashraf Reda <ashraf.reda.m@gmail.com>
2021-12-27 13:52:22 +0200

    fix header height on resize

--- assets/js/partials/initate.js
+++ assets/js/partials/initate.js
@@ -53,10 +53,10 @@ var headerWrapper = document.getElementById('site-header-outer'),
 var headerHeight = headerContent.clientHeight;
 headerWrapper.style.height = headerHeight + 'px';
 
-// window.addEventListener('resize', function () {
-//     headerHeight = headerContent.clientHeight;
-//     headerWrapper.style.height = headerHeight + 'px';
-// });
+window.addEventListener('resize', function () {
+    headerHeight = headerContent.clientHeight;
+    headerWrapper.style.height = headerHeight + 'px';
+});
 
 window.addEventListener('scroll', function () {
     var headerOffsetTop = headerWrapper.offsetTop;
