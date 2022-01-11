d131bd42cbfaf30ec0d223b20cdf1c58302da544
Jamal Al-Matari <jalmatari@gmail.com>
2022-01-11 21:50:48 +0300

    🚧 WPI

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
             "assets/js/**/*.js",
             //todo:: inject it via the plugin or easier way
             'node_modules/@salla.sa/twilight-tailwind-theme/safe-list-css.txt',
+            // 'node_modules/@salla.sa/twilight-components/src/components/**/*.tsx',
         ]
     },
     darkMode: 'class', // or 'media' or 'class'
