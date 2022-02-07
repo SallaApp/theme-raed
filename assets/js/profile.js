cc2237c9db3a62a43066a32b2ac2e1fb92a8c746
Adel Elshafey <eng.adelelshafey@gmail.com>
2022-02-07 10:42:20 +0200

    Move login component outside header container
    - Remove tel-input form profile to use new component
    - Update tailwind.config

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,13 +1,13 @@
 import Flatpickr from 'flatpickr';
 import BasePage from './base-page';
-import TelInput from "intl-tel-input";
+// import TelInput from "intl-tel-input";
 
 class Profile extends BasePage {
     onReady() {
         Flatpickr('.date-element', {"dateFormat": "Y-m-d"});
         this.initiateProfileImage();
         this.appendAvtar();
-        this.initTelInput();
+        // this.initTelInput();
     }
 
     initiateProfileImage() {
@@ -46,27 +46,27 @@ class Profile extends BasePage {
         };
     }
 
-    initTelInput() {
-        salla.document.event.onKeyup('.tel-input', event => salla.helpers.digitsOnly(event.target));
-        document.querySelectorAll('.tel-input').forEach(intlInput => {
-            salla.helpers.digitsOnly(intlInput);
-            let iti = TelInput(intlInput, {
-                initialCountry    : intlInput.dataset.code || 'sa',
-                preferredCountries: ['sa', 'ae', 'kw', 'bh', 'qa', 'iq', 'om', 'ye', 'eg', 'jo', 'sy', 'ps', 'sd', 'lb', 'dz', 'tn', 'ma', 'ly'],
-                formatOnDisplay   : false,
-                separateDialCode  : true,
-                autoPlaceholder   : 'aggressive',
-                utilsScript       : 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.min.js',
-            });
-            intlInput.addEventListener("countrychange", () => {
-                let data = iti.getSelectedCountryData();
-                document.querySelectorAll('.country_code').forEach(input => input.value = data.iso2.toUpperCase());
-                document.querySelectorAll('.country_key').forEach(input => {
-                    input.value = ('+' + data.dialCode).replace('++', '+');
-                });
-            });
-        });
-    }
+    // initTelInput() {
+    //     salla.document.event.onKeyup('.tel-input', event => salla.helpers.digitsOnly(event.target));
+    //     document.querySelectorAll('.tel-input').forEach(intlInput => {
+    //         salla.helpers.digitsOnly(intlInput);
+    //         let iti = TelInput(intlInput, {
+    //             initialCountry    : intlInput.dataset.code || 'sa',
+    //             preferredCountries: ['sa', 'ae', 'kw', 'bh', 'qa', 'iq', 'om', 'ye', 'eg', 'jo', 'sy', 'ps', 'sd', 'lb', 'dz', 'tn', 'ma', 'ly'],
+    //             formatOnDisplay   : false,
+    //             separateDialCode  : true,
+    //             autoPlaceholder   : 'aggressive',
+    //             utilsScript       : 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.min.js',
+    //         });
+    //         intlInput.addEventListener("countrychange", () => {
+    //             let data = iti.getSelectedCountryData();
+    //             document.querySelectorAll('.country_code').forEach(input => input.value = data.iso2.toUpperCase());
+    //             document.querySelectorAll('.country_key').forEach(input => {
+    //                 input.value = ('+' + data.dialCode).replace('++', '+');
+    //             });
+    //         });
+    //     });
+    // }
 }
 
 Profile.className = 'Profile';
