c42df74060a758b22acfd6ff0d7a0535dc52214b
Jamal Al-Matari <jalmatari@gmail.com>
2022-03-08 01:14:18 +0300

    ✨ Enhancements

--- assets/js/thankyou.js
+++ assets/js/thankyou.js
@@ -2,46 +2,11 @@ import BasePage from './base-page';
 
 class ThankYou extends BasePage {
     onReady() {
-        this.initiateForm();
-        app.anime('.thanks-item', {translateX: [20, 0]});
-    }
-
-    registerEvents() {
-        app.onClick('.btn--copy', function(){app.copyLinkToClipboard(this.id)} );
-        
-        app.onClick('#btn-open-order', event => {
-            //important for mobile apps
-            salla.event.dispatch('mobile::order.placed', {order_id: event.target.dataset.orderId});
-            location.href = event.target.dataset.orderUrl;
-        });
-    }
-
-    initiateForm() {
-        let email = document.getElementById('resend-invoice-email');
-        //there is form for resend invoice email, so no need for logic, if there is no email input
-        if (!email) {
-            return;
-        }
-        let btn = document.getElementById('resend-invoice-submit');
-
-        email.addEventListener('keyup', () => this.isValidEmail(email.value) && email.parentElement.classList.remove('error'));
-
         //added By data-on-success="onSuccess", data-on-fail="onFail"
-        window.onFail = window.onSuccess = () => btn.classList.remove('btn--has-loading');
-
-        //added By data-filter-before-submit="beforeSubmit"
-        window.beforeSubmit = (form_data, that, event) => {
-            if (!app.isValidEmail(email.value)) {
-                email.parentElement.classList.add('error');
-                throw 'Not Valid';
-            }
-
-            email.parentElement.classList.remove('error');
-            btn.classList.add('btn--has-loading');
-            btn.setAttribute('disabled', true);
-            return form_data;
-        };
+        window.onFail = window.onSuccess = () => document.getElementById('resend-invoice-submit').classList.remove('btn--has-loading');
+        app.anime('.thanks-item', {translateX: [20, 0]});
+        app.onClick('.btn--copy', event => app.copyLinkToClipboard(event.target.id));
     }
 }
 
-ThankYou.intiateWhenReady('ThankYou',['thank-you']);
+ThankYou.intiateWhenReady('ThankYou', ['thank-you']);
