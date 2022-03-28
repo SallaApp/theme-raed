d2037ae130ab0ace7689d162287afa47d4bd146f
Ashraf Reda <ashraf.reda.m@gmail.com>
2022-03-28 10:50:33 +0200

    thankyou page: sent invoice message fixes

--- assets/js/thankyou.js
+++ assets/js/thankyou.js
@@ -3,7 +3,11 @@ import BasePage from './base-page';
 class ThankYou extends BasePage {
     onReady() {
         app.anime('.thanks-item', {translateX: [20, 0]});
-        salla.order.event.onSent(res => document.querySelector('#invoice-form').innerHTML = res.message);
+        let form = document.querySelector('#invoice-form');
+        salla.order.event.onSent(res =>{
+            form.innerHTML = res.message;
+            form.classList.add('sent');
+        });
     }
 }
 
