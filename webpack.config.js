506d10516b28818daa5f018398418991a7d25bd3
Abdullah Al-taheri <abdullahaltahery@gmail.com>
2022-05-24 20:14:03 +0300

    🎉 Add websocket hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new ThemeWatcher(),
+        new ThemeWatcher(env.websocketURL),
         new MiniCssExtractPlugin(),
         new WebpackBuildNotifierPlugin({
             title: "Salla Theme",
