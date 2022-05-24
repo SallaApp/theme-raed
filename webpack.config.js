7b11485461343d7c4a1d6d5f755409ff5b82dd88
Abdullah Al-taheri <abdullahaltahery@gmail.com>
2022-05-24 20:15:38 +0300

    adding env

--- webpack.config.js
+++ webpack.config.js
@@ -4,100 +4,102 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 
 
-module.exports = {
-    entry  : {
-        app     : ['./assets/styles/app.scss', './assets/js/wishlist.js', './assets/js/app.js'],
-        home    : './assets/js/home.js',
-        checkout: ['./assets/js/cart.js', './assets/js/thankyou.js'],
-        pages   : ['./assets/js/blog.js', './assets/js/brands.js',],
-        product : ['./assets/js/product.js', './assets/js/products.js'],
-        customer: [
-            './assets/js/profile.js',
-            './assets/js/order.js',
-        ],
+module.exports = (env) => {
+    return {
+        entry  : {
+            app     : ['./assets/styles/app.scss', './assets/js/wishlist.js', './assets/js/app.js'],
+            home    : './assets/js/home.js',
+            checkout: ['./assets/js/cart.js', './assets/js/thankyou.js'],
+            pages   : ['./assets/js/blog.js', './assets/js/brands.js',],
+            product : ['./assets/js/product.js', './assets/js/products.js'],
+            customer: [
+                './assets/js/profile.js',
+                './assets/js/order.js',
+            ],
 
-        //plugins
-        filepond : ['./assets/styles/05-utilities/filepond.scss', './assets/js/partials/filepond.js'],
-        flatpickr: './assets/styles/05-utilities/flatpicker.scss',
-        //fonts
-        // amazon_ember: './assets/fonts/amazon_ember.scss',
-        // apple: './assets/fonts/apple.scss',
-        // dubai: './assets/fonts/dubai.scss',
-        // estedad: './assets/fonts/estedad.scss',
-    },
-    output : {
-        path : path.resolve(__dirname, "assets", "dist"),
-        clean: true,
-    },
-    stats  : {
-        modules                : false,
-        assetsSort             : "size",
-        assetsSpace            : 50,
-        cachedAssets           : true,
-        groupAssetsByPath      : false,
-        groupAssetsByExtension : false,
-        groupAssetsByEmitStatus: false,
-        groupAssetsByChunk     : false,
-        groupAssetsByInfo      : false,
-        relatedAssets          : true,
-        performance            : true,
-    },
-    module : {
-        rules: [
-            {
-                test   : /\.js$/,
-                exclude: /(node_modules)/,
-                use    : {
-                    loader : 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                            ["@babel/plugin-transform-runtime",
-                             {
-                                 "regenerator": true
-                             }
-                            ]
-                        ],
+            //plugins
+            filepond : ['./assets/styles/05-utilities/filepond.scss', './assets/js/partials/filepond.js'],
+            flatpickr: './assets/styles/05-utilities/flatpicker.scss',
+            //fonts
+            // amazon_ember: './assets/fonts/amazon_ember.scss',
+            // apple: './assets/fonts/apple.scss',
+            // dubai: './assets/fonts/dubai.scss',
+            // estedad: './assets/fonts/estedad.scss',
+        },
+        output : {
+            path : path.resolve(__dirname, "assets", "dist"),
+            clean: true,
+        },
+        stats  : {
+            modules                : false,
+            assetsSort             : "size",
+            assetsSpace            : 50,
+            cachedAssets           : true,
+            groupAssetsByPath      : false,
+            groupAssetsByExtension : false,
+            groupAssetsByEmitStatus: false,
+            groupAssetsByChunk     : false,
+            groupAssetsByInfo      : false,
+            relatedAssets          : true,
+            performance            : true,
+        },
+        module : {
+            rules: [
+                {
+                    test   : /\.js$/,
+                    exclude: /(node_modules)/,
+                    use    : {
+                        loader : 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                ["@babel/plugin-transform-runtime",
+                                {
+                                    "regenerator": true
+                                }
+                                ]
+                            ],
+                        }
                     }
+                },
+                {
+                    test: /\.(s(a|c)ss)$/,
+                    use : [
+                        MiniCssExtractPlugin.loader,
+                        {loader: "css-loader", options: {url: false}},
+                        "postcss-loader",
+                        "sass-loader",
+                    ]
+                },
+            ],
+        },
+        plugins: [
+            new ThemeWatcher(env.websocketURL),
+            new MiniCssExtractPlugin(),
+            new WebpackBuildNotifierPlugin({
+                title: "Salla Theme",
+                logo : path.resolve("./assets/images/favicon/apple-icon.png"),
+                // suppressSuccess: true, // don't spam success notifications
+                successSound: false,
+            }), {
+                apply: (compiler) => {
+                    if (compiler.options.mode === 'development' && !compiler.options.watch) {
+                        return;
+                    }
+                    compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                        let themeSettingsPath = path.resolve('./theme.json');
+                        let themeSettings = require(themeSettingsPath);
+                        let version = (themeSettings.version || '0.0.0').split('.').reverse();
+                        version[0] = Number(version[0]) + 1;
+                        themeSettings.version = version.reverse().join(".");
+                        require('fs').writeFileSync(
+                            themeSettingsPath,
+                            JSON.stringify(themeSettings, null, '  ')
+                        );
+                    });
                 }
-            },
-            {
-                test: /\.(s(a|c)ss)$/,
-                use : [
-                    MiniCssExtractPlugin.loader,
-                    {loader: "css-loader", options: {url: false}},
-                    "postcss-loader",
-                    "sass-loader",
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new ThemeWatcher(env.websocketURL),
-        new MiniCssExtractPlugin(),
-        new WebpackBuildNotifierPlugin({
-            title: "Salla Theme",
-            logo : path.resolve("./assets/images/favicon/apple-icon.png"),
-            // suppressSuccess: true, // don't spam success notifications
-            successSound: false,
-        }), {
-            apply: (compiler) => {
-                if (compiler.options.mode === 'development' && !compiler.options.watch) {
-                    return;
-                }
-                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                    let themeSettingsPath = path.resolve('./theme.json');
-                    let themeSettings = require(themeSettingsPath);
-                    let version = (themeSettings.version || '0.0.0').split('.').reverse();
-                    version[0] = Number(version[0]) + 1;
-                    themeSettings.version = version.reverse().join(".");
-                    require('fs').writeFileSync(
-                        themeSettingsPath,
-                        JSON.stringify(themeSettings, null, '  ')
-                    );
-                });
             }
-        }
-    ],
+        ],
+    }
 }
-;
+
