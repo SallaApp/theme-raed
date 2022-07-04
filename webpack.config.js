ab6890398155c442b68f48c346e2aade8e574937
Ashraf Reda <ashraf.reda.m@gmail.com>
2022-07-04 11:30:01 +0200

    regenerator-runtime fix

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
                     loader : 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
+                        plugins: [
+                          ["@babel/plugin-transform-runtime",
+                           {
+                               "regenerator": true
+                           }
+                          ]
+                        ],
                     }
                 }
             },
