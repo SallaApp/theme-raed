66916d13eabd9bad5888989c77d00e3109e8cb91
Abdullah Al-taheri <abdullahaltahery@gmail.com>
2022-07-05 22:11:57 +0300

    Passing env to twilight-js plugin

--- webpack.config.js
+++ webpack.config.js
@@ -7,55 +7,57 @@ const asset = file => path.resolve('src/assets', file || '');
 const output = file => path.resolve("public", file || '');
 
 
-module.exports = {
-    entry  : {
-        app     : [asset('styles/app.scss'), asset('js/wishlist.js'), asset('js/app.js')],
-        home    : asset('js/home.js'),
-        checkout: [asset('js/cart.js'), asset('js/thankyou.js')],
-        pages   : [asset('js/blog.js'), asset('js/brands.js'),],
-        product : [asset('js/product.js'), asset('js/products.js')],
-        customer: [asset('js/profile.js'), asset('js/order.js')],
+module.exports = (env) => {
+    return {
+        entry  : {
+            app     : [asset('styles/app.scss'), asset('js/wishlist.js'), asset('js/app.js')],
+            home    : asset('js/home.js'),
+            checkout: [asset('js/cart.js'), asset('js/thankyou.js')],
+            pages   : [asset('js/blog.js'), asset('js/brands.js'),],
+            product : [asset('js/product.js'), asset('js/products.js')],
+            customer: [asset('js/profile.js'), asset('js/order.js')],
 
-        //plugins
-        filepond : [asset('styles/05-utilities/filepond.scss'), asset('js/partials/filepond.js')],
-        flatpickr: asset('styles/05-utilities/flatpicker.scss')
-    },
-    output : {path: output(), clean: true},
-    stats  : {modules: false, assetsSort: "size", assetsSpace: 50},
-    module : {
-        rules: [
-            {
-                test   : /\.js$/,
-                exclude: /(node_modules)/,
-                use    : {
-                    loader : 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                          ["@babel/plugin-transform-runtime",
-                           {
-                               "regenerator": true
-                           }
-                          ]
-                        ],
+            //plugins
+            filepond : [asset('styles/05-utilities/filepond.scss'), asset('js/partials/filepond.js')],
+            flatpickr: asset('styles/05-utilities/flatpicker.scss')
+        },
+        output : {path: output(), clean: true},
+        stats  : {modules: false, assetsSort: "size", assetsSpace: 50},
+        module : {
+            rules: [
+                {
+                    test   : /\.js$/,
+                    exclude: /(node_modules)/,
+                    use    : {
+                        loader : 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                            ["@babel/plugin-transform-runtime",
+                            {
+                                "regenerator": true
+                            }
+                            ]
+                            ],
+                        }
                     }
-                }
-            },
-            {
-                test: /\.(s(a|c)ss)$/,
-                use : [
-                    MiniCssExtractPlugin.loader,
-                    {loader: "css-loader", options: {url: false}},
-                    "postcss-loader",
-                    "sass-loader",
-                ]
-            },
+                },
+                {
+                    test: /\.(s(a|c)ss)$/,
+                    use : [
+                        MiniCssExtractPlugin.loader,
+                        {loader: "css-loader", options: {url: false}},
+                        "postcss-loader",
+                        "sass-loader",
+                    ]
+                },
+            ],
+        },
+        plugins: [
+            new ThemeWatcher(env),
+            new MiniCssExtractPlugin(),
+            new CopyPlugin({patterns: [{from: asset('images'), to: output('images')}]}),
         ],
-    },
-    plugins: [
-        new ThemeWatcher(),
-        new MiniCssExtractPlugin(),
-        new CopyPlugin({patterns: [{from: asset('images'), to: output('images')}]}),
-    ],
+    }
 }
 ;
