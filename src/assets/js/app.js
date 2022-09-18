f5fe68d8e1c8f448b1ec58c6c111f3b784062af9
Jamal Al-Matari <jalmatari@gmail.com>
2022-09-18 14:23:01 +0300

    exclude twilight from bunlde (#51)
    Co-authored-by: Salah Alkhwlani <alkhwlani@yandex.com>

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,3 @@
-// import '@salla.sa/twilight';
-// import {applyPolyfills, defineCustomElements as SallaWebComponents} from '@salla.sa/twilight-components/loader';
-// applyPolyfills().then(() => {
-//     SallaWebComponents(window);
-// });
-
 import MobileMenu from 'mmenu-light';
 import Swal from 'sweetalert2';
 import Anime from './partials/anime';
