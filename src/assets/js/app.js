7b8797fab9126c2ec95fa747f94549169213716e
ibra-hassan <ibra.96.hassan@gmail.com>
2022-09-25 15:21:48 +0300

    Hotfix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -21,7 +21,7 @@ class App extends salla.AppHelpers {
         // TODO: delete after doing a better solution for that.
         salla.money = (money) => {
             let currency = Salla.config.currency(money?.currency).symbol;
-            money = typeof money === 'number' ? money : money.amount;
+            money = typeof money === 'object' ? money.amount : money;
             return salla.helpers.number(money) + ' ' + currency;
         };
 
