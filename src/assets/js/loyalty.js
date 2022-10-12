dec0e84f62cbf0865bc008b7d2a4a59970b0fa2c
Ashraf Reda <ashraf.reda.m@gmail.com>
2022-10-12 11:54:43 +0200

    Loyalty fixes and enhanments

--- src/assets/js/loyalty.js
+++ src/assets/js/loyalty.js
@@ -4,32 +4,34 @@ import Slider from './partials/slider'
 class Loyalty extends BasePage {
     onReady() {
         let count = app.element(".count-anime")?.dataset?.count||0;
-        (new anime.timeline()).add({
-          targets: '.star-anime',
-          opacity: [0, 1],
-          scale: [0, 1],
-          rotate: [25, 0],
-          duration: 2000
-        })
-        .add({
-            targets: '.loyalty__banner-content .info > *',
-            opacity: [0, 1],
-            translateX: [20, 0],
-            duration: 800,
-            delay: (el, i) => i * 100,
-        }, '-=1000')
+        (new anime.timeline())
+        // .add({
+        //   targets: '.star-anime',
+        //   opacity: [0, 1],
+        //   scale: [0, 1],
+        //   rotate: [25, 0],
+        //   duration: 2000
+        // })
+        // .add({
+        //     targets: '.loyalty__banner-content .info > *',
+        //     opacity: [0, 1],
+        //     translateX: [20, 0],
+        //     duration: 800,
+        //     delay: (el, i) => i * 100,
+        // }, '-=1000')
         .add({
             targets: `.count-anime`,
             innerText: [0, count],
+            duration: 2000,
             easing: "linear",
             round: true,
-        }, '-=1000')
-        .add({
-            targets: '.btn-anime',
-            opacity: [0, 1],
-            duration: 2000,
-            translateX: [20, 0]
-        }, '-=1000')
+        })
+        // .add({
+        //     targets: '.btn-anime',
+        //     opacity: [0, 1],
+        //     duration: 2000,
+        //     translateX: [20, 0]
+        // })
 
 
         /*
