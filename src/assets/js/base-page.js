b2276d817f2b19e5374053c89de2671d2db1bf2f
ibrahim gabr <ibrahim.G@salla.sa>
2022-10-26 23:16:28 +0200

    revert relay on twilight::ready event for js pages

--- src/assets/js/base-page.js
+++ src/assets/js/base-page.js
@@ -16,16 +16,31 @@ class BasePage {
  */
 BasePage.initiateWhenReady = function (className, allowedPages = null) {
     salla.onReady(() => {
-        if (allowedPages && !allowedPages.includes(salla.config.get('page.slug'))) {
-            app.log(className + ' Skiped.');
-            return;
-        }
-
-        window.pageClass = new this;
-        pageClass.onReady();
-        pageClass.registerEvents();
-        app.log(`${className} Loaded🎉`);
-    })
-}
+        let tries = 0, inerval;
+        //check if theme app is initiated each 0.1 sec for one sec otherwise don't load current page class
+        (new Promise((resolve, reject) =>
+            inerval = setInterval(function () {
+                if (window.app?.isThemeAppReady()) {
+                    resolve(true);
+                    clearInterval(inerval);
+                }
+                if (tries > 10) {
+                    reject('Failed to Find `window.app`😢');
+                    clearInterval(inerval);
+                }
+                tries++;
+            }, 100)
+        )).then(() => {
+            if (allowedPages && !allowedPages.includes(salla.config.get('page.slug'))) {
+                app.log(className + ' Skiped.');
+                return;
+            }
+            window.pageClass = new this;
+            pageClass.onReady();
+            pageClass.registerEvents();
+            app.log(`${className} Loaded🎉`);
+        });
+    });
+};
 
 export default BasePage;
