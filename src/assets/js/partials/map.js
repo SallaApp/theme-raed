51a3d678047dbe6212e95a55a1d64342028b3208
ahmed dhafer <59390233+dhafer95@users.noreply.github.com>
2022-11-15 13:16:09 +0300

    changing the new product option to web component

--- src/assets/js/partials/map.js
+++ src/assets/js/partials/map.js
@@ -1,18 +1,19 @@
 const MAPS = document.querySelectorAll('#location_map');
 
 MAPS.forEach(map => {
-  const locationInputGeocode = document.getElementById(map.dataset.inputGeocode);
+
+  const savedLocation = map.dataset.location;
   
   // Seting the location if there is a value
-  if (locationInputGeocode.value) {
-    const latlngStr = locationInputGeocode.value.split(',');
+  if (savedLocation) {
+    const latlngStr = savedLocation.split(',');
     map.setAttribute('lat', parseFloat(latlngStr[0]))
     map.setAttribute('lng', parseFloat(latlngStr[1]))
   }
 
   // Apend the value to the input for submiting 
   map.addEventListener('selected', (event) => {
-    locationInputGeocode.value = `${event.detail.lat}, ${event.detail.lng}`;
+    locationInput.value = `${event.detail.lat}, ${event.detail.lng}`;
   });
 });
 
