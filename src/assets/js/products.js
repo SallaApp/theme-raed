ad38fe0e1907e60ab8b7a52210b2a93bb0729fd3
Ahmed Abd ElRazek Ahmed <ahmed@salla.sa>
2022-11-15 05:29:19 +0200

    fix

--- src/assets/js/products.js
+++ src/assets/js/products.js
@@ -3,13 +3,13 @@ import BasePage from './base-page';
 class Products extends BasePage {
     onReady() {
         // Sort Products
-        app.on('change','#product-filter', event =>
-          window.location.href = salla.helpers.addParamToUrl('by', event.target.value)
+        themeApp.on('change','#product-filter', event =>
+            window.location.href = salla.helpers.addParamToUrl('by', event.target.value)
         );
     }
 }
 
-Products.initiateWhenReady([
+Products.initiateWhenReady( [
     'product.index',
     'product.index.latest',
     'product.index.offers', 'product.index.search',
