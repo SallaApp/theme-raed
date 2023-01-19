c368d5a97347a98d4a2e90cf79685442a248a1e3
eslamoo <essomgd@gmail.com>
2023-01-19 15:01:48 +0200

    Handle buy all btn

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -5,7 +5,15 @@ window.fslightbox = Lightbox;
 
 class Landing extends BasePage {
     onReady() {
+        let buyAllBtn = app.element(".buy-all-btn")
+        buyAllBtn.addEventListener('click', () => {
+            buyAllBtn.load()
+            .then(() => buyAllBtn.disable())
+            .then(() => salla.landing.createCartFromPage(buyAllBtn.dataset.landingId))
+            .finally(()=> setTimeout(() => { buyAllBtn.stop() && buyAllBtn.enable() }, 1000))
+        })
     }
+    
 }
 
 Landing.initiateWhenReady(['landing-page']);
