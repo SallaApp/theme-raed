bb55d03ab8f03b685a28f2230a0531e6fe580dc6
eslamoo <essomgd@gmail.com>
2023-03-06 14:35:48 +0200

    Build Assets

--- public/colorpicker.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll("#color_picker").forEach((function(e){var t=document.getElementById(e.dataset.colorInput);t.value&&e.setAttribute("color",t.value),e.addEventListener("submitted",(function(e){t.value="".concat(e.detail.hex)}))}));
