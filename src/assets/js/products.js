2941ee630b4456989a680ed59966057fa1de23f0
Eslam Magdy <32875906+eslamoo@users.noreply.github.com>
2023-03-26 13:24:17 +0200

    🐞 Fix sortBy filter (#200)

--- src/assets/js/products.js
+++ src/assets/js/products.js
@@ -7,9 +7,10 @@ class Products extends BasePage {
             app.element('#product-filter').value = urlParams.get('sort');
         }
         // Sort Products
-        app.on('change','#product-filter', event =>{
+        app.on('change','#product-filter', async event =>{
             window.history.replaceState(null, null, "?sort=" + event.currentTarget.value);
-            //todo:: use it like this: app.element('salla-products-list').sortBy=event.currentTarget.value;
+            app.element('salla-products-list').sortBy=event.currentTarget.value;
+            await app.element('salla-products-list').reload();
             app.element('salla-products-list').setAttribute('filters', `{"sort": "${event.currentTarget.value}"}`)
         });
     }
