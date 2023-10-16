452cf2d89294e64e7549e702f9b0236153d50716
eslamoo <essomgd@gmail.com>
2023-10-16 12:31:21 +0300

    Enhance quick buy button in landing page

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -4,21 +4,7 @@ import Lightbox from "fslightbox";
 window.fslightbox = Lightbox;
 
 class Landing extends BasePage {
-    onReady() {
-        let buyAllBtn = app.element(".buy-all-btn")
-        buyAllBtn.addEventListener('click', () => {
-            buyAllBtn.load()
-                .then(() => buyAllBtn.disable())
-                .then(() => salla.landing.createCartFromPage(buyAllBtn.dataset.landingId))
-                .finally(() => setTimeout(() => { buyAllBtn.stop() && buyAllBtn.enable() }, 1000))
-        })
-
-        salla.wishlist.event.onAdditionFailed(() => {
-            if (salla.config.isGuest()) {
-                salla.event.dispatch('login::open')
-            }
-        });
-    }
+    onReady() { }
 
     
 
