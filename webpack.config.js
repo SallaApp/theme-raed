39d2df2b2519d8f6f88cb4656eeca76af1b1096d
Ashraf Reda <ashraf.reda.m@gmail.com>
2023-10-29 15:19:43 +0200

    ✨ Introducing Mini-Checkout to the Landing Page

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     entry  : {
         app     : [asset('styles/app.scss'), asset('js/wishlist.js'), asset('js/app.js')],
         home    : asset('js/home.js'),
+        'product-card' : asset('js/partials/product-card.js'),
         checkout: [asset('js/cart.js'), asset('js/thankyou.js')],
         pages   : [asset('js/loyalty.js'), asset('js/brands.js'),],
         product : [asset('js/product.js'), asset('js/products.js')],
