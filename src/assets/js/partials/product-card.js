ccbe83f62a8b33ab3903b761fc54dc5d585b0733
Ashraf Reda <ashraf.reda.m@gmail.com>
2023-11-21 16:09:08 +0200

    Fixes collections 1 (#267)
    Co-authored-by: ahmed-salah <ahmed.s@salla.sa>

--- src/assets/js/partials/product-card.js
+++ src/assets/js/partials/product-card.js
@@ -252,7 +252,7 @@ class ProductCard extends HTMLElement {
             ${this.product?.donation?.can_donate ? '' : this.getProductPrice()}
             ${this.product?.rating?.stars && !this.minimal ?
               `<div class="s-product-card-rating">
-                <i class="sicon-star2"></i>
+                <i class="sicon-star2 text-orange-400"></i>
                 <span>${this.product.rating.stars}</span>
               </div>`
                : ``}
