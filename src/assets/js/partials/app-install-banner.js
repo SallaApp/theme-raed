cf6ae662ca2713bc5a8310dc9a3496bfdce580bb
AHMED <ahmedbasalib.17@gmail.com>
2023-11-26 22:29:48 +0300

    feat(app-install-banner): init the ui styling

--- src/assets/js/partials/app-install-banner.js
+++ src/assets/js/partials/app-install-banner.js
@@ -9,12 +9,27 @@ class AppInstall extends HTMLElement {
 	}
   
 	onReady(){
+		this.logo = salla.config.get('store.logo');
+		this.name = salla.config.get('store.name');
+		this.position = false ? 'top-0' : 'bottom-0';
+		this.title = 'جربت تطبيق سلة ؟';
+		this.sub_title = 'قم بتحميل التطبيق وأحصل على خصومات وعروض تصل لـ 70%';
+		this.cta_text = ' حمله الان';
 		
 		this.render()
 	}
   
 	render(){
-	  this.innerHTML = `<h1>Hello I ma the app install banner :)</h1>`
+	  this.innerHTML = `<div class="flex flex-row items-center fixed ${this.position} left-0 right-0 w-[95%] my-4 mx-auto bg-[#ffe4cc] z-[999] rounded-md p-3 gap-3">
+	  <div>
+	    <img src=${this.logo} width="58" height="58" alt="${this.name}">
+	  </div>
+	  <div>
+	    <h2 class="text-base font-bold text-primary">${this.title}</h2>
+		<span class="text-sm font-normal">${this.sub_title}<u class="text-primary">${this.cta_text}</u></span>
+	  </div>
+	  <span class="absolute top-2 left-3"><i class="sicon-cancel"></i></span>
+	  </div>`
 		
 	}
   }
