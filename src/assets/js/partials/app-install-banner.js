dadd96fa16da3af6c5ba55c83b74d460e4d0efc8
AHMED <ahmedbasalib.17@gmail.com>
2023-12-03 15:53:13 +0300

    refactor: skip api request and use twilight

--- src/assets/js/partials/app-install-banner.js
+++ src/assets/js/partials/app-install-banner.js
@@ -2,123 +2,108 @@
  * Salla app install banner component.
  */
 class AppInstall extends HTMLElement {
-	connectedCallback() {
-		salla.onReady(() => {
-			// Early check to avoid unneeded render and api request
-			// do not render the component if the device in not (tablet/mobile) or
-			// if the store does not have apps or banner was dismissed by the user
-			if (
-				!this.isMobileOrTabletDevice() ||
-				!salla.config.get('store.apps') ||
-				localStorage.getItem('bannerDismissed') === 'true'
-			)
-				return;
+  connectedCallback() {
+    salla.onReady(() => {
+      // Early check to avoid unneeded render and api request
+      // do not render the component if the device in not (tablet/mobile) or
+      // if the store does not have apps or banner was dismissed by the user
+      // or if the banner was disabled by the store admin
+      if (
+        !this.isMobileOrTabletDevice() ||
+        !salla.config.get('store.apps') ||
+        localStorage.getItem('bannerDismissed') === 'true' ||
+        !salla.config.get('store.app_install_prompt')
+      )
+        return;
 
-			// fetch the banner data
-			this.fetchTheAppMarketingData();
-			// TODO: replace it with deep links when they are ready
-			this.cta_link =
-				this.getMobileOS() === 'iOS'
-					? salla.config.get('store.apps.appstore')
-					: salla.config.get('store.apps.googleplay');
-		});
-	}
+      // set the banner data
+      this.data = salla.config.get('store.app_install_prompt');
+      this.renderTheBanner();
+      // TODO: replace it with deep links when they are ready
+      this.cta_link =
+        this.getMobileOS() === 'iOS'
+          ? salla.config.get('store.apps.appstore')
+          : salla.config.get('store.apps.googleplay');
+    });
+  }
 
-	renderTheBanner() {
-		this.render();
-		setTimeout(() => {
-			this.setAttribute('open', true);
-		}, 3000);
-	}
+  renderTheBanner() {
+    this.render();
+    setTimeout(() => {
+      this.setAttribute('open', true);
+    }, 3000);
+  }
 
-	setData(data) {
-		this.position = data.position === 'top' ? 'top' : 'bottom';
-		this.title = data.title;
-		this.sub_title = data.sub_title;
-	}
+  /**
+   * Get the OS of device (android/ios).
+   * @returns {string} returns iOS/Android/Other
+   */
+  getMobileOS = () => {
+    const ua = navigator.userAgent;
+    if (/android/i.test(ua)) {
+      return 'Android';
+    } else if (
+      /iPad|iPhone|iPod/.test(ua) ||
+      (/Macintosh/i.test(navigator.userAgent) && 'ontouchend' in document)
+    ) {
+      return 'iOS';
+    }
+    return 'Other';
+  };
 
-	/**
-	 * Get the OS of device (android/ios).
-	 * @returns {string} returns iOS/Android/Other
-	 */
-	getMobileOS = () => {
-		const ua = navigator.userAgent;
-		if (/android/i.test(ua)) {
-			return 'Android';
-		} else if (
-			/iPad|iPhone|iPod/.test(ua) ||
-			(/Macintosh/i.test(navigator.userAgent) && 'ontouchend' in document)
-		) {
-			return 'iOS';
-		}
-		return 'Other';
-	};
+  /**
+   * Check if the website opens from mobile or tablet devices only (android/ios).
+   *
+   * @param {number} screen the width of biggest screen to be checked
+   * @returns {boolean} true if it is mobile or tablet else false
+   */
+  isMobileOrTabletDevice = (screen = 1024) => {
+    const screenWidth = window.innerWidth <= screen;
+    const userAgentCheck = /Macintosh|Android|iPhone|iPad|iPod/i.test(
+      navigator.userAgent
+    );
+    const hasTouch =
+      'ontouchstart' in window ||
+      'ontouchend' in document ||
+      navigator.maxTouchPoints > 0;
 
-	/**
-	 * Check if the website opens from mobile or tablet devices only (android/ios).
-	 *
-	 * @param {number} screen the width of biggest screen to be checked
-	 * @returns {boolean} true if it is mobile or tablet else false
-	 */
-	isMobileOrTabletDevice = (screen = 1024) => {
-		const screenWidth = window.innerWidth <= screen;
-		const userAgentCheck = /Macintosh|Android|iPhone|iPad|iPod/i.test(
-			navigator.userAgent
-		);
-		const hasTouch =
-			'ontouchstart' in window ||
-			'ontouchend' in document ||
-			navigator.maxTouchPoints > 0;
+    return userAgentCheck && screenWidth && hasTouch;
+  };
 
-		return userAgentCheck && screenWidth && hasTouch;
-	};
+  closeBanner() {
+    localStorage.setItem('bannerDismissed', true);
+    if (this.position === 'top') {
+      this.setAttribute('open', false);
+    } else {
+      // handle closing animation first, then close the banner
+      this.setAttribute('closing', '');
+      this.addEventListener(
+        'animationend',
+        () => {
+          this.removeAttribute('closing');
+          this.setAttribute('open', false);
+        },
+        { once: true }
+      );
+    }
+  }
 
-	fetchTheAppMarketingData() {
-		salla.api.request('http://localhost:3000/data', 'get').then((res) => {
-			if (res) {
-				// render the banner if it is enabled
-				if (res.is_enabled) {
-					this.setData(res);
-					this.renderTheBanner();
-				}
-			}
-		});
-	}
-
-	closeBanner() {
-		localStorage.setItem('bannerDismissed', true);
-		if (this.position === 'top') {
-			this.setAttribute('open', false);
-		} else {
-			// handle closing animation first, then close the banner
-			this.setAttribute('closing', '');
-			this.addEventListener(
-				'animationend',
-				() => {
-					this.removeAttribute('closing');
-					this.setAttribute('open', false);
-				},
-				{ once: true }
-			);
-		}
-	}
-
-	render() {
-		this.setAttribute('open', false);
-		this.setAttribute('position', this.position);
-		this.classList.add('s-app-install-banner');
-		this.innerHTML = `
+  render() {
+    this.setAttribute('open', false);
+    this.setAttribute('position', this.data.position);
+    this.classList.add('s-app-install-banner');
+    this.innerHTML = `
 	    <div>
-	      <img src=${salla.config.get(
-					'store.logo'
-				)} width="58" height="58" alt="${salla.config.get('store.name')}">
+	      <img src=${this.data.icon} width="58" height="58" alt="${salla.config.get(
+      'store.name'
+    )}">
 	    </div>
 	    <div>
-	      <h2 class="s-app-install-banner-title">${this.title}</h2>
-		  <p class="s-app-install-banner-sub-title">${this.sub_title}
+	      <h2 class="s-app-install-banner-title">${this.data.title}</h2>
+		  <p class="s-app-install-banner-sub-title">${this.data.sub_title}
 		    <a href="${
-					this.cta_link
-				}" target="_blank" aria-label="download app" class="s-app-install-banner-cta">
+          this.cta_link
+        }" target="_blank" aria-label="download app" class="s-app-install-banner-cta">
 			${salla.lang.get('blocks.footer.download_apps')}
 			</a>
 		  </p>
@@ -126,7 +111,7 @@ class AppInstall extends HTMLElement {
 	    <button onclick="this.parentElement.closeBanner()" 
 	      class="s-app-install-banner-cancel-button"><i class="sicon-cancel"></i>
 	    </button>`;
-	}
+  }
 }
 
 customElements.define('salla-app-install-banner', AppInstall);
