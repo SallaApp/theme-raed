c58c2d884a4583680b5e51d0a13c90e4f05475d7
Ahmed Omar Khamis Basalib <ahmedbasalib.17@gmail.com>
2023-12-18 01:14:58 +0300

    fix(themes): code formating

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -21,15 +21,15 @@ class Product extends BasePage {
     }
 
     initImagesZooming() {
-		    // skip if the screen is not desktop or if glass magnifier
-		    // is already crated for the image before
-		    const imageZoom = document.querySelector('.image-slider .magnify-wrapper.swiper-slide-active .img-magnifier-glass');
-		    if (window.innerWidth  < 1024 || imageZoom) return;
-		    setTimeout(() => {
-			      // set delay after the resizing is done, start creating the glass
-			      // to create the glass in the proper position
-			      const image = document.querySelector('.image-slider .swiper-slide-active img');
-			      zoom(image?.id, 2);
+        // skip if the screen is not desktop or if glass magnifier
+        // is already crated for the image before
+        const imageZoom = document.querySelector('.image-slider .magnify-wrapper.swiper-slide-active .img-magnifier-glass');
+        if (window.innerWidth  < 1024 || imageZoom) return;
+        setTimeout(() => {
+            // set delay after the resizing is done, start creating the glass
+            // to create the glass in the proper position
+            const image = document.querySelector('.image-slider .swiper-slide-active img');
+            zoom(image?.id, 2);
         }, 250);
 		
 
@@ -40,8 +40,8 @@ class Product extends BasePage {
 			
                 // if the zoom glass is already created skip
                 if (window.innerWidth  < 1024 || imageZoom) return;
-				        const image = document.querySelector('.image-slider .magnify-wrapper.swiper-slide-active img');
-				        zoom(image?.id, 2);
+                const image = document.querySelector('.image-slider .magnify-wrapper.swiper-slide-active img');
+                zoom(image?.id, 2);
             }, 250)
         })
     }
