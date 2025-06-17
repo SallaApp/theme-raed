eb0aa1938673e4f8fba4dff3dfc28795c9596c89
ahmed-salah <ahmed.s@salla.sa>
2025-06-17 12:42:35 +0300

    fix(themes): upgrade packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': true },
-    },
+    '@tailwindcss/postcss': {},
   }
 }
